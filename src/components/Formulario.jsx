abdc96d4e96f462ef2e8de9758760c09b671de6c
Jair <jairaceves56@gmail.com>
2023-03-16 19:36:24 -0600

    Mostramos los Años

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+import { MARCAS, YEARS } from '../constants'
 
 export const Formulario = () => {
   return (
@@ -11,7 +12,37 @@ export const Formulario = () => {
                     name="marca"
                     className="w-full p-3 bg-white border border-gray-300 rounded-lg"
                 >
-                    <option value="">-- Selecciona --</option>
+                    <option value="">-- Selecciona Marca --</option>
+
+                    {MARCAS.map(marca => (
+                        <option
+                            key={marca.id}
+                            value={marca.id}
+                        >
+                            {marca.nombre}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="my-5">
+                <label className="block mb-3 font-bold text-gray-400 uppercase">
+                    Año
+                </label>
+                <select 
+                    name="marca"
+                    className="w-full p-3 bg-white border border-gray-300 rounded-lg"
+                >
+                    <option value="">-- Selecciona Año --</option>
+
+                    {YEARS.map(year => (
+                        <option
+                            key={year}
+                            value={year}
+                        >
+                            {year}
+                        </option>
+                    ))}
                 </select>
             </div>
         </form>
