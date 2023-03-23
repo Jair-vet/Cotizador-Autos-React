007b16291279cf0d32318cc37bdf89827f279fd4
Jair <jairaceves56@gmail.com>
2023-03-23 09:39:32 -0600

    Mostramos el Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,13 +1,37 @@
 import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES } from '../constants'
 
 export const Resultado = () => {
 
-    const { resultado } = useCotizador()
+    const { resultado, datos } = useCotizador()
+    const { marca, plan, year } = datos
+
+    // Filtramos por el valor del Id
+    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
+    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
+
     if( resultado === 0 ) return null
 
   return (
-    <>
-        {resultado}
-    </>
+    <div className="bg-gray-300 text-center mt-5 p-5 shadow rounded-lg">
+        <h2 className="text-emerald-600 font-black text-3xl">Resumen</h2>
+
+        <p className="my-2">
+            <span className="text-emerald-700 font-bold text-xl uppercase">Marca: </span>
+            {nombreMarca.nombre}
+        </p>
+        <p className="my-2">
+            <span className="text-emerald-700 font-bold text-xl uppercase">Plan: </span>
+            {nombrePlan.nombre}
+        </p>
+        <p className="my-2">
+            <span className="text-emerald-700 font-bold text-xl uppercase">Year: </span>
+            {year}
+        </p>
+        <p className="my-2 text-2xl">
+            <span className="text-emerald-700 font-bold text-xl uppercase">Total </span>
+            {resultado}
+        </p>
+    </div>
   )
 }
